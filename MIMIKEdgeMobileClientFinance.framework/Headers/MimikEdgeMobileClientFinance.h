7a52fbf16689a25a480618946ab7c48dd736c339
Raduz Benicky <raduz.benicky@mimik.com>
2021-02-22 13:18:38 -0800

    2.0 Re Release of MIMIKEdgeMobileClientFinance as a separate client library.

---
+++ MIMIKEdgeMobileClientFinance.framework/Headers/MimikEdgeMobileClientFinance.h
@@ -0,0 +1,22 @@
+//
+//  MIMIKEdgeMobileClientFinance.h
+//  MIMIKEdgeMobileClientFinance
+//
+//  Created by Radúz Benický on 2020-02-11.
+//  Copyright © 2020 mimik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MIMIKEdgeMobileClientFinance.
+FOUNDATION_EXPORT double MIMIKEdgeMobileClientFinanceVersionNumber;
+
+//! Project version string for MIMIKEdgeMobileClientFinance.
+FOUNDATION_EXPORT const unsigned char MIMIKEdgeMobileClientFinanceVersionString[];
+
+#import <UIKit/UIKit.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <edgeSDK_iOS/PublicHeader.h>
+#if defined(IMPORT_EDGE_HEADER_FILE)
+#endif
+
